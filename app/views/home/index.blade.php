d59e054fba78efddc036fa490b6ceb749c695557
Gilang Prasetia <gilang.prasetia@wgs.co.id>
2014-10-08 14:22:42 +0700

    Separated home page to partial template

--- app/views/home/index.blade.php
+++ app/views/home/index.blade.php
@@ -1,173 +1,130 @@
-@extends('layouts/default')
+@include('layouts/default')
 
-@section('content')
+  <section class="map">
+    <div class="border-top"></div>
+    <div id="map" class="map-canvas"></div>
+    <div class="container">
+      <a class="logo" href=""><img src="{{ Theme::asset('img/logo.png') }}"></a>
+      <div class="top-nav">
+        <div class="right-side">
+          <p>2013</p>
+          <div class="dropdown">
+            <a href="#" class="hamburger"></a>
+          </div>
+        </div>
+      </div>
+      <div class="dropshadow">
+        <img src="{{ Theme::asset('img/dropshadow.png') }}">
+        <div class="search-wrp">
+          <div class="col-md-3">
+            <a href="#" id="category">
+              <img src="{{ Theme::asset('img/add.png') }}" />
+              <span id="select_category_label">{{Lang::get('frontend.select_category')}}</span>
+            </a>
+            <div class="dropdown-path">
+              <ul class="dropdown-scroll">
+                <li><a onclick='select_category(1)' id="select_category_id_1" >Default Category</a></li>
+              </ul>
+              <span class="arrow-down"></span>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <a href="#" id="question">
+              <img src="{{ Theme::asset('img/add.png') }}" />
+              <span id="select_question_label">{{Lang::get('frontend.select_question')}}</span>
+            </a>
+            <div class="dropdown-path">
+              <ul class="dropdown-scroll">
+                <li><a onclick='select_question(1)' id="select_question_id_1" >Persepsi Mengenai Pemilu</a></li>
+                <li><a onclick='select_question(2)' id="select_question_id_2" >Kebutuhan pendidikan kewarganegaraan</a></li>
+                <li><a onclick='select_question(3)' id="select_question_id_3" >Pemahaman mengenai daftar pemilih</a></li>
+              </ul>
+              <span class="arrow-down"></span>
+            </div>
+          </div>
+          <div class="col-md-3"><a class="find-surveys" href="#" onclick='find_survey()'>{{Lang::get('frontend.find_surveys')}} <img src="{{ Theme::asset('img/arrow.png') }}"></a></div>
+        </div>
+      </div>
+  </section>
 
-<section id="home" name="home"></section>
-	<div id="headerwrap">
-	    <div class="container">
-	    	<div class="row centered">
-	    		<div class="col-lg-12">
-					<h1>Welcome To <b>{{ Setting::meta_data('general', 'name')->value }}</b></h1>
-					<h3>Show your product with this handsome theme.</h3>
-					<br>
-	    		</div>
-	    		
-	    		<div class="col-lg-2">
-	    			<h5>Amazing Results</h5>
-	    			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-	    			<img class="hidden-xs hidden-sm hidden-md" src="{{ Theme::asset('img/arrow1.png') }}">
-	    		</div>
-	    		<div class="col-lg-8">
-	    			<img class="img-responsive" src="{{ Theme::asset('img/app-bg.png') }}" alt="">
-	    		</div>
-	    		<div class="col-lg-2">
-	    			<br>
-	    			<img class="hidden-xs hidden-sm hidden-md" src="{{ Theme::asset('img/arrow2.png') }}">
-	    			<h5>Awesome Design</h5>
-	    			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-	    		</div>
-	    	</div>
-	    </div> <!--/ .container -->
-	</div><!--/ #headerwrap -->
+  <section class="filter">
+    <div class="container">
+      <div class="col-md-12">
+        <ul>
+          <li>
+            <a href="#">
+              <img src="{{ Theme::asset('img/filter.png') }}">
+              <span>{{Lang::get('frontend.cycle')}}</span>
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <img src="{{ Theme::asset('img/filter.png') }}">
+              <span>{{Lang::get('frontend.filter_by_age')}}</span>
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <img src="{{ Theme::asset('img/filter.png') }}">
+              <span>{{Lang::get('frontend.filter_by_income')}}</span>
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <img src="{{ Theme::asset('img/filter.png') }}">
+              <span>{{Lang::get('frontend.filter_by_education')}}</span>
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <img src="{{ Theme::asset('img/filter.png') }}">
+              <span>{{Lang::get('frontend.filter_by_gender')}}</span>
+            </a>
+          </li>
+          <li>
+            <a class="clear-all" onclick='clear_all_filter()' href="#">{{Lang::get('frontend.clear_all')}}</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </section>
 
+  <section class="survey-pemilu">
+    <div class="container center">
+      <div class="col-md-12">
+        <h1>Hasil Survey Pemilu 2013/2014</h1>
+        <h3>{{Lang::get('frontend.survey_question')}}</h3>
+        <p>Alasan utama yang menyebabkan orang-orang tidak <br/>mengikuti Pemilihan Presiden pada bulan Juli 2014</p>
+        <div class="chart">
+          <div class="col-md-5"><div id="chartContainerPie" style="height: 300px; width: 100%;"></div></div>
+          <div class="col-md-7"><div id="chartContainer" style="height: 300px; width: 100%;"></div></div>
+          <div class="col-md-12">
+            <ul class="chart-pagination">
+              <li><a href="#" class="orange-bg"><img src="{{ Theme::asset('img/arrow-l.png') }}"></a></li>
+              <li><a href="#" class="orange-bg">{{Lang::get('frontend.compare_this_survey')}}</a></li>
+              <li><a href="#" class="orange-bg"><img src="{{ Theme::asset('img/arrow.png') }}"></a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 
-	<section id="desc" name="desc"></section>
-	<!-- INTRO WRAP -->
-	<div id="intro">
-		<div class="container">
-			<div class="row centered">
-				<h1>Designed To Excel</h1>
-				<br>
-				<br>
-				<div class="col-lg-4">
-					<img src="{{ Theme::asset('img/intro01.png') }}" alt="">
-					<h3>Community</h3>
-					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-				</div>
-				<div class="col-lg-4">
-					<img src="{{ Theme::asset('img/intro02.png') }}" alt="">
-					<h3>Schedule</h3>
-					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-				</div>
-				<div class="col-lg-4">
-					<img src="{{ Theme::asset('img/intro03.png') }}" alt="">
-					<h3>Monitoring</h3>
-					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-				</div>
-			</div>
-			<br>
-			<hr>
-	    </div> <!--/ .container -->
-	</div><!--/ #introwrap -->
-	
-	<!-- FEATURES WRAP -->
-	<div id="features">
-		<div class="container">
-			<div class="row">
-				<h1 class="centered">What's New?</h1>
-				<br>
-				<br>
-				<div class="col-lg-6 centered">
-					<img class="centered" src="{{ Theme::asset('img/mobile.png') }}" alt="">
-				</div>
-				
-				<div class="col-lg-6">
-					<h3>Some Features</h3>
-					<br>
-				<!-- ACCORDION -->
-		            <div class="accordion ac" id="accordion2">
-		                <div class="accordion-group">
-		                    <div class="accordion-heading">
-		                        <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseOne">
-		                        First Class Design
-		                        </a>
-		                    </div><!-- /accordion-heading -->
-		                    <div id="collapseOne" class="accordion-body collapse in">
-		                        <div class="accordion-inner">
-								Great Framework
-								</div><!-- /accordion-inner -->
-		                    </div><!-- /collapse -->
-		                </div><!-- /accordion-group -->
-		                <br>
-		
-		                <div class="accordion-group">
-		                    <div class="accordion-heading">
-		                        <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseTwo">
-		                        Retina Ready Theme
-		                        </a>
-		                    </div>
-		                    <div id="collapseTwo" class="accordion-body collapse">
-		                        <div class="accordion-inner">
-								<p>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-								</div><!-- /accordion-inner -->
-		                    </div><!-- /collapse -->
-		                </div><!-- /accordion-group -->
-		                <br>
-		
-		                 <div class="accordion-group">
-		                    <div class="accordion-heading">
-		                        <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseThree">
-		                        Awesome Support
-		                        </a>
-		                    </div>
-		                    <div id="collapseThree" class="accordion-body collapse">
-		                        <div class="accordion-inner">
-								<p>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-								</div><!-- /accordion-inner -->
-		                    </div><!-- /collapse -->
-		                </div><!-- /accordion-group -->
-		                <br>
-		                
-		                 <div class="accordion-group">
-		                    <div class="accordion-heading">
-		                        <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseFour">
-		                        Responsive Design
-		                        </a>
-		                    </div>
-		                    <div id="collapseFour" class="accordion-body collapse">
-		                        <div class="accordion-inner">
-								<p>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-								</div><!-- /accordion-inner -->
-		                    </div><!-- /collapse -->
-		                </div><!-- /accordion-group -->
-		                <br>			
-					</div><!-- Accordion -->
-				</div>
-			</div>
-		</div><!--/ .container -->
-	</div><!--/ #features -->
-	
-	
-	<section id="showcase" name="showcase"></section>
-	<div id="showcase">
-		<div class="container">
-			<div class="row">
-				<h1 class="centered">Some Screenshots</h1>
-				<br>
-				<div class="col-lg-8 col-lg-offset-2">
-					<div id="carousel-example-generic" class="carousel slide">
-					  <!-- Indicators -->
-					  <ol class="carousel-indicators">
-					    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-					    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-					  </ol>
-					
-					  <!-- Wrapper for slides -->
-					  <div class="carousel-inner">
-					    <div class="item active">
-					      <img src="{{ Theme::asset('img/item-01.png') }}" alt="">
-					    </div>
-					    <div class="item">
-					      <img src="{{ Theme::asset('img/item-02.png') }}" alt="">
-					    </div>
-					  </div>
-					</div>
-				</div>
-			</div>
-			<br>
-			<br>
-			<br>	
-		</div><!-- /container -->
-	</div>	
-
-@stop
+  <section class="compare-survey">
+    <div class="container">
+      <div class="col-md-6">
+        <h4>Lorem ipsum dolor sit amet, consectet</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ipsum risus, dignissim vel arcu sit amet, sodales dignissim nibh. Suspendisse lobortis neque sed est sollicitudin ornare.</p>
+      </div>
+      <div class="col-md-6">
+        <img src="{{ Theme::asset('img/compare.png') }}">
+        <div>
+          <h4>{{Lang::get('frontend.compare_survey_results')}}</h4>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ipsum risus</p>
+          <a href="" class="orange-bg">{{Lang::get('frontend.compare_survey')}}</a>
+        </div>
+      </div>
+    </div>
+  </section>
+  
+@include('layouts/footer')
