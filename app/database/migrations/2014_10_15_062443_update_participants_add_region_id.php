fcae40d4cbf6ec5d23d76c507d48bcd1fc5a8f55
Gilang Prasetia <gilang.prasetia@wgs.co.id>
2014-10-15 13:27:31 +0700

    Add migrations region_id participants

---
+++ app/database/migrations/2014_10_15_062443_update_participants_add_region_id.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateParticipantsAddRegionId extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+    Schema::table('participants', function(Blueprint $table)
+    {
+      $table->integer('region_id');
+      $table->dropColumn('participant_id');
+    });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+    Schema::table('questioners', function(Blueprint $table)
+    {
+      $table->dropColumn('region_id');
+      $table->integer('participant_id');
+    });
+	}
+
+}
