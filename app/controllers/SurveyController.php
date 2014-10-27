70a9fa5fa9fdcc4fb57a060e24ead57bf0e85fb4
Irfan Fauzan <irfan.fauzan@wgs.co.id>
2014-10-27 09:55:18 +0700

    finishing import data

--- app/controllers/SurveyController.php
+++ app/controllers/SurveyController.php
@@ -226,7 +226,97 @@ class SurveyController extends AvelcaController {
 	}
 
 	public function postOversample(){
-		return "I am here";
+		$categories = Input::get('unselected');
+		$oversample = Input::get('header');
+
+		$oversample_exist = array_search($oversample[0], $categories);
+		if($oversample_exist)
+		{
+			unset($categories[$oversample_exist]);
+			$categories = array_values($categories);
+		}
+
+		foreach($categories as $category){
+			$arr_category = explode(';',$category);
+
+			$s_code = Code::where('code', '=', $arr_category[0])->first();
+			if(!isset($s_code))
+			{
+				$code = Code::create(array('code' => $arr_category[0]));
+
+				if($code)
+				{
+					$s_category = Category::where('name', '=', $arr_category[1])->first();
+					if(!isset($s_category))
+					{
+						Category::create(array('name' => $arr_category[1], 'code_id' => $code->id));
+					}
+				}
+			}
+		}
+
+		$cycle = Cycle::where('id', '=', Input::get('id_cycle'))->first();
+		$filename = 'uploads/'.$cycle->excel_file;
+		Excel::selectSheetsByIndex(1)->filter('chunk')->load($filename)->chunk(250, function($results)
+		{
+			$data = array();
+			$oversample = Input::get('header');
+			$arr_data = $results->toArray();
+			$region_id = 0; 
+			$sample_type = null;
+
+			foreach($arr_data as $data)
+			{
+				foreach($data as $key => $value)
+				{
+					$code = Code::where('code', '=', $key)->first();
+					if(isset($code))
+					{
+
+						$s_region = Region::join('codes', 'regions.code_id', '=', 'codes.id')->where('code', '=', strtoupper($key))->first();
+						if(isset($s_region))
+						{
+							$region_id = $s_region->id;
+						}
+						else
+						{
+							$s_category_item = CategoryItem::where('name', '=', $value)->first();
+							if(!isset($s_category_item))
+							{
+								if($value != "")
+								{
+									$category_item = CategoryItem::create(array('name' => $value, 'category_id' => $code->id));
+									$category_id = $category_item->id;	
+								}
+							}	
+						}
+					}
+
+					$arr_oversample = explode(';',$oversample[0]);
+					if(strtolower($arr_oversample[0]) == $key){
+						$sample_type = 1;
+						$arr_value = explode('. ', $value);
+						if(strtolower($arr_value[1]) == 'sample utama'){
+							$sample_type = 0;
+							
+						}
+					}
+
+					$question = Question::where('code', '=', $key)->first();
+					if(isset($question))
+					{
+						if($value != "")
+						{
+							$answer = Answer::create(array('answer' => $value, 'question_id' => $question->id, 'cycle_id' => Input::get('id_cycle')));
+
+							$question_participant = QuestionParticipant::create(array('answer_id' => $answer->id, 'region_id' => $region_id, 'sample_type' => $sample_type));
+						}
+					}
+				}
+			}
+
+		});
+
 	}
 
 	public function Import()
