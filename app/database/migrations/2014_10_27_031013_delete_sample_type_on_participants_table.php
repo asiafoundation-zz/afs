a4943a0f5a69890881f65675e1e3dfe1877b82c4
Irfan Fauzan <irfan.fauzan@wgs.co.id>
2014-10-27 10:13:04 +0700

    delete sample_type on participants table

--- app/database/migrations/2014_10_27_031013_delete_sample_type_on_participants_table.php
+++ app/database/migrations/2014_10_27_031013_delete_sample_type_on_participants_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class DeleteParticipantTable extends Migration {
+class DeleteSampleTypeOnParticipantsTable extends Migration {
 
 	/**
 	 * Run the migrations.
@@ -12,7 +12,9 @@ class DeleteParticipantTable extends Migration {
 	 */
 	public function up()
 	{
-		Schema::drop('participants');
+		Schema::table('participants', function($table){
+			$table->dropColumn('sample_type');
+		});
 	}
 
 	/**
@@ -22,7 +24,9 @@ class DeleteParticipantTable extends Migration {
 	 */
 	public function down()
 	{
-		//
+		Schema::table('participants', function($table){
+			$table->integer('sample_type');
+		});
 	}
 
 }
