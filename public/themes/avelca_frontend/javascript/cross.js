72605a6d0952d904388e8e883c0bd144be39eb0c
Irfan Fauzan <irfan.fauzan@wgs.co.id>
2014-11-06 07:37:27 +0700

    modify cross js

--- public/themes/avelca_frontend/javascript/cross.js
+++ public/themes/avelca_frontend/javascript/cross.js
@@ -10,14 +10,14 @@ $(document).ready(function(){
     $('.chart-flag').hide();
   })
 
-  $('.select-category').change(function(){
-      console.log($(this).val());
-  });
+  /*$('.select-category').change(function(){
+      
+  });*/
 
   $('.select-question').change(function(){
       console.log($(this).val());
 
-      $('.submit-cross').data('question_id', $(this).val());
+      $('.submit-cross').data('question_id', $(this).val()); //send data-question_id to button
   });
 
   function numAttrs(obj) {
@@ -32,7 +32,7 @@ $(document).ready(function(){
 
   $('.submit-cross').click(function(){  
     var question_row = $(this).data('question_id');
-    var $table = $($('#get-cross-table').html());
+    var $table = $($('#get-cross-table').html()); //inisiate js template
 
     $('.cross-table').html("");
 
@@ -44,9 +44,9 @@ $(document).ready(function(){
         'question_row' : question_row
       },
       success : function(data){
-        var count_value = 0;
-        
+        var count_value = 0; //inisiate variable for question header count
 
+        //show question header
         $('#question_header', $table).append(data.question_headers[0]['question']);
         $.each(data.question_headers, function(index, value){
           $('#answer_header', $table).append('<th>'+ value['answer'] +'</th>');
@@ -55,18 +55,19 @@ $(document).ready(function(){
 
         $('#question_header', $table).attr('colspan',count_value);
 
+        //show question row
         $.each(data['question_rows'], function(index, value){
-          result = '<tr><td width="20%">'+ value['answer'] +'</td>';
+          result = '<tr><td width="20%">'+ value['answer'] +'</td>'; //create html for showing question row data
           for(i=0;i<count_value;i++){
             result += '<td align="center">'+ value['result'+i] +'</td>';
           }
           result += '</tr>';
 
-          $('#answer_row', $table).append(result);
+          $('#answer_row', $table).append(result); //append html to tempate
 
         });
         
-        $('.cross-table').append($table);    
+        $('.cross-table').append($table); //append template to cross-table class
       }
 
     })
