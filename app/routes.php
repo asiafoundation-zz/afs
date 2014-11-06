9c8feb14d2607e7bbe3a38354c59a5da8b34e2fb
Irfan Fauzan <irfan.fauzan@wgs.co.id>
2014-11-06 07:11:13 +0700

    register new class to route

--- app/routes.php
+++ app/routes.php
@@ -73,33 +73,4 @@ Route::group(array('prefix' => LaravelLocalization::setLocale(), 'before' => 'fr
 	Route::get('filter-select', 'HomeController@filterSelect');
 });
 
-Route::post('cross', function(){
-		
-		$question_headers = Answer::select(DB::raw('count(answers.answer) as count_header, questions.question as question, questions.id as question_id, answers.id as id, answers.answer as answer'))
-							->join('questions', 'questions.id', '=', 'answers.question_id')
-							->where('questions.id', '=', Input::get('question_header'))
-							->groupBy('answers.answer')
-							->get();
-							
-		$query = "answers.answer as answer,";
-		$counter = 0;
-		foreach($question_headers as $header){
-			$query .= "(
-							count(answers.answer) + ". $header->count_header ."
-						) as 'result$counter',";
-
-			$counter ++;
-		}
-
-		$query = rtrim($query, ',');
-
-		$question_rows = Answer::select(
-							DB::raw($query)
-						)
-						->join('questions', 'questions.id', '=', 'answers.question_id')
-						->where('questions.id', '=', Input::get('question_row'))
-						->groupBy('answer')
-						->get()->toArray();
-		
-		return array('question_rows' => $question_rows, 'question_headers' => $question_headers);
-	});
+Route::post('cross', 'AnswerController@postCross');
