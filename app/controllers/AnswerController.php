d3b62bda20552fda250b2b62d4ed344337079bcb
Irfan Fauzan <irfan.fauzan@wgs.co.id>
2014-11-06 07:10:16 +0700

    Create class for cross tab vie in controller

--- app/controllers/AnswerController.php
+++ app/controllers/AnswerController.php
@@ -6,5 +6,36 @@ class AnswerController extends AvelcaController {
 	{
 		parent::__construct($Model);
 	}
+
+	public function postCross()
+	{
+		$question_headers = Answer::select(DB::raw('count(answers.answer) as count_header, questions.question as question, questions.id as question_id, answers.id as id, answers.answer as answer'))
+							->join('questions', 'questions.id', '=', 'answers.question_id')
+							->where('questions.id', '=', Input::get('question_header'))
+							->groupBy('answers.answer')
+							->get();
+							
+		$query = "answers.answer as answer,";
+		$counter = 0;
+		foreach($question_headers as $header){
+			$query .= "(
+							count(answers.answer) + ". $header->count_header ."
+						) as 'result$counter',";
+
+			$counter ++;
+		}
+
+		$query = rtrim($query, ',');
+
+		$question_rows = Answer::select(
+							DB::raw($query)
+						)
+						->join('questions', 'questions.id', '=', 'answers.question_id')
+						->where('questions.id', '=', Input::get('question_row'))
+						->groupBy('answer')
+						->get()->toArray();
+		
+		return array('question_rows' => $question_rows, 'question_headers' => $question_headers);
+	}
 		
 }
