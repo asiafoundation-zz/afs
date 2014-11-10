b2a889c59aca12ea92df215f4f03173755ab2fe9
Irfan Fauzan <irfan.fauzan@wgs.co.id>
2014-11-10 17:24:50 +0700

    implement multi table cross data to js

--- public/themes/avelca_frontend/javascript/cross.js
+++ public/themes/avelca_frontend/javascript/cross.js
@@ -33,7 +33,6 @@ $(document).ready(function(){
 
   $('.submit-cross').click(function(){  
     var question_row = $(this).data('question_id');
-    var $table = $($('#get-cross-table').html().trim()); //inisiate js template
 
     $('.cross-table').html("");
 
@@ -45,30 +44,37 @@ $(document).ready(function(){
         'question_row' : question_row
       },
       success : function(data){
-        var count_value = 0; //inisiate variable for question header count
-
-        //show question header
-        $('#question_header', $table).append(data.question_headers[0]['question']);
-        $.each(data.question_headers, function(index, value){
-          $('#answer_header', $table).append('<th>'+ value['answer'] +'</th>');
-          count_value++;
-        });
-
-        $('#question_header', $table).attr('colspan',count_value);
-
-        //show question row
-        $.each(data['question_rows'], function(index, value){
-          result = '<tr><td width="20%">'+ value['answer'] +'</td>'; //create html for showing question row data
-          for(i=0;i<count_value;i++){
-            result += '<td align="center">'+ value['result'+i] +'</td>';
-          }
-          result += '</tr>';
-
-          $('#answer_row', $table).append(result); //append html to tempate
-
-        });
+        var count_array = Object.keys(data.question_headers).length;
         
-        $('.cross-table').append($table); //append template to cross-table class
+        
+        for(var a=0;a<count_array;a++){
+          var count_value = 0; //inisiate variable for question header count
+          
+          //show question header  
+          var $table = $($('#get-cross-table').html().trim()); //inisiate js template
+          $('#question_header', $table).append(data.question_headers[a][0]['question']);
+          $.each(data.question_headers[a], function(index, value){
+            $('#answer_header', $table).append('<th>'+ value['answer'] +'</th>');
+            count_value++;
+          });
+
+          $('#question_header', $table).attr('colspan',count_value);
+
+          //show question row
+          $.each(data.question_rows[a], function(index, value){
+            result = '<tr><td width="20%">'+ value['answer'] +'</td>'; //create html for showing question row data
+            for(i=0;i<count_value;i++){
+              result += '<td align="center">'+ value['result'+i] +'</td>';
+            }
+            result += '</tr>';
+
+            $('#answer_row', $table).append(result); //append html to tempate
+
+          });
+          
+          $('.cross-table').append($table); //append template to cross-table class
+        }
+
       }
 
     });
