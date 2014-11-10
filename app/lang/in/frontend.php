9f33f87c149e26d21a5ee42cc3e87a7fefe8b18c
Irfan Fauzan <irfan.fauzan@wgs.co.id>
2014-11-10 17:25:10 +0700

    add new indonesian language

---
+++ app/lang/in/frontend.php
@@ -0,0 +1,27 @@
+<?php
+
+return array(
+
+	/* Basic Features */
+
+	"select_category" => "Pilih Kategori",
+	"select_question" => "Pilih Pertanyaan",
+	"find_surveys" => "Temukan Survey",
+	"filter_by_age" => "Umur",
+	"filter_by_income" => "Pendapatan",
+	"filter_by_education" => "Pendidikan",
+	"filter_by_gender" => "Jenis Kelamin",
+	"clear_all" => "Bersihkan Semua",
+	"cycle" => "Pilih cycle",
+	"compare_this_survey" => "Bandingkan dengan survey lain",
+	"return" => "Kembali",
+	"compare_survey_results" => "Bandingkan survey",
+	"compare_survey_results_content" => "Bandingkan hasil survey dengan cara lain",
+	"survey_question" => "Pertanyaan pada survey",
+	"compare_survey" => "BANDINGKAN SURVEY",
+	"empty_data" => "Tidak ada data survey",
+	"comparing_cycle" => "Membandingkan",
+	"cross_by_another_variable" => "Silang data",
+	"cross_by_another_variable_p" => "Silang data untuk membandingkan jawaban dari dua pertanyaan dalam survey yang sama",
+	"filter_participant_by" => "Saring partisipan dengan"
+);
