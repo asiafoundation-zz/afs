b9368ea971baa64b20c856fe57ff92f94e38e50c
Gilang Prasetia <gilang.prasetia@wgs.co.id>
2014-11-12 10:26:39 +0700

    Recommit:Fix default questions

--- app/models/Survey.php
+++ app/models/Survey.php
@@ -232,9 +232,9 @@ class Survey extends Eloquent {
 				}
 			}
 			// Set default question
-			$is_default_exist = Question::where('questions.is_default','=',1)->get();
+			$is_default_exist = Question::where('questions.is_default','=',1)->first();
 			
-			if (isset($is_default_exist)) {
+			if (!isset($is_default_exist)) {
 				$default_question = Question::join('question_categories', 'question_categories.id','=','questions.question_category_id')->join('answers', 'answers.question_id','=','questions.id')->where('question_categories.survey_id','=',$survey->id)->orderBy('questions.id', 'DESC')->first();
 				$default_question->is_default = 1;
 				$default_question->save();
