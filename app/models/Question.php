2a4c18f9112cf6e30ce7139b2a685fce682687b2
Gilang Prasetia <gilang.prasetia@wgs.co.id>
2014-12-18 09:56:59 +0700

    remove invalid character from answers

--- app/models/Question.php
+++ app/models/Question.php
@@ -205,7 +205,10 @@ class Question extends Eloquent {
 
 		// Count index label percentage
 		foreach ($questions as $key_questions => $question) {
-			$question->answer = trim(preg_replace('/\s\s+/', ' ', $question->answer));
+			$dataval = preg_replace('/[^A-Za-z0-9\-\s?\/#$%^&*()+=\-\[\],.:<>|]\n\r/', '', $question->answer);
+			$dataval = str_replace('"', "", $dataval);
+			
+			$question->answer = trim(preg_replace('/\s\s+/', ' ', $dataval));
 			$question->indexlabel = !$total_amount ? 0 : round(($question->amount / $total_amount) * 100,2);
 		}
 		// sort array based on amounts
